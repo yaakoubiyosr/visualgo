9c985f42f82eb847dd61ca4cfdeec5e35e348a8d
yosr <yassourayaakoubi@gmail.com>
2024-09-19 09:03:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+
+
+// https://visualgo.net/en/array?slide=6-1
+// let arr=[7,8,5,1,3,9,4]
+// for(let i=arr.length-1;i>=1;i--){
+//     for(let j =0;j<arr.length;j++){
+//         if(arr[j]<arr[i]){
+//             var aux=arr[i]
+//             arr[i]=arr[j]
+//             arr[j]=aux
+        
+//         }
+//     }
+// }
+// console.log(arr)
+
+
+// https://visualgo.net/en/bst
+
+// for (let i=0;i <arr.length ; i++){
+//     for(let j=0 ; j<arr.length-1; j++){
+//         if(arr[j]>arr[j+1]){
+//             var aux=arr[i]
+//                         var aux=arr[j]
+//                         arr[j]=arr[j+1]
+//                         arr[j+1]=aux
+//         }
+//     }
+// }
+// console.log(arr)
+
+
+
+
+
+
